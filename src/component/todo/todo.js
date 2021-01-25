b91aa7cfd0f571f95d0eec3b5de2500a7c58a46d
Bikram <bikrambiswal2030@gmail.com>
2021-01-25 21:44:06 +0530

    todo app css added

--- src/component/todo/todo.js
+++ src/component/todo/todo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../style.css";
 
 export default class Todo extends React.Component {
   constructor(props) {
@@ -35,68 +36,74 @@ export default class Todo extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Todo App</h1>
-        <input
-          style={{
-            borderRadius: 10,
-            height: 30,
-            width: 175,
-            outline: "none",
-            marginRight: 20
-          }}
-          placeholder="Enter The Task"
-          value={this.state.taskInput}
-          onChange={this.handleInput}
-        />
-        <button
-          disabled={this.state.taskInput === "" ? true : false}
-          onClick={this.handlePush}
-        >
-          Add Task
-        </button>
-
-        {this.state.tasksArr.length === 0 ? (
-          <h2>You have No tasks to do</h2>
-        ) : (
-          <h2>Tasks to do</h2>
-        )}
-        <div>
-          <ul>
-            {this.state.tasksArr.map((single, index) => {
-              return (
-                <li>
-                  {single}
-                  <br />
-                  <button
-                    onClick={(e) => {
-                      this.handleComplete(e, index, single);
-                    }}
-                  >
-                    Completed
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-
-        {this.state.tasksCompleted.length === 0 ? (
-          <h2>No Task Completed</h2>
-        ) : (
-          <h2>Tasks Completed</h2>
-        )}
-        <div>
-          <ul>
-            {this.state.tasksCompleted.map((single, index) => {
-              return (
-                <li>
-                  {single}
-                  <br />
-                </li>
-              );
-            })}
-          </ul>
+      <div id="todo">
+        <center>
+          <h1>Todo App</h1>
+          <input
+            style={{
+              borderRadius: 10,
+              height: 30,
+              width: 175,
+              outline: "none",
+              marginRight: 20
+            }}
+            placeholder="Enter The Task"
+            value={this.state.taskInput}
+            onChange={this.handleInput}
+          />
+          <button
+            disabled={this.state.taskInput === "" ? true : false}
+            onClick={this.handlePush}
+          >
+            Add Task
+          </button>
+        </center>
+        <div class="float-container">
+          <div class="float-child">
+            {this.state.tasksArr.length === 0 ? (
+              <h2>You have No tasks to do</h2>
+            ) : (
+              <h2>Tasks to do</h2>
+            )}
+            <div class="blue">
+              <ul>
+                {this.state.tasksArr.map((single, index) => {
+                  return (
+                    <li>
+                      {single}
+                      <br />
+                      <button
+                        onClick={(e) => {
+                          this.handleComplete(e, index, single);
+                        }}
+                      >
+                        Completed
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+          <div class="float-child2">
+            {this.state.tasksCompleted.length === 0 ? (
+              <h2>No Task Completed</h2>
+            ) : (
+              <h2>Tasks Completed</h2>
+            )}
+            <div class="green">
+              <ul>
+                {this.state.tasksCompleted.map((single, index) => {
+                  return (
+                    <li>
+                      {single}
+                      <br />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     );
