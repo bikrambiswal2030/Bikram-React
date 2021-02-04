40a1e40cca296154e9f0ef7122c7a835de6d4872
Bikram <bikrambiswal2030@gmail.com>
2021-02-04 15:22:55 +0530

    complete API Assignment

--- src/component/Api/delay.js
+++ src/component/Api/delay.js
@@ -12,17 +12,22 @@ import {
   Snackbar,
   ListItem,
   ListItemAvatar,
-  ListItemText
+  ListItemText,
+  CircularProgress,
+  Paper
 } from "@material-ui/core";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import LinearProgress from "@material-ui/core/LinearProgress";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import BrightnessHighIcon from "@material-ui/icons/BrightnessHigh";
+
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 export default class Delay extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       users: "",
-      progress: 10
+      progress: 10,
+      dark: false
     };
   }
   componentDidMount() {
@@ -33,58 +38,77 @@ export default class Delay extends React.Component {
       });
     });
   }
+  toggleTheme = () => {
+    this.setState({
+      dark: !this.state.dark
+    });
+  };
 
   render() {
     return (
-      <Grid container xs={12}>
-        <Grid item container justify="center">
-          <Typography align="center" color="primary" variant="h5">
-            USERS
-          </Typography>
-        </Grid>
+      <ThemeProvider
+        theme={createMuiTheme({
+          palette: {
+            type: this.state.dark ? "dark" : "light"
+          }
+        })}
+      >
+        <Paper style={{ height: "100vh" }}>
+          <Grid container xs={12}>
+            <Grid item xs={12}>
+              <Typography align="center" color="primary" variant="h5">
+                USERS
+              </Typography>
+              <Button onClick={this.toggleTheme}>
+                {this.state.dark ? <BrightnessHighIcon /> : <Brightness4Icon />}
+                {this.state.dark ? "LIGHT MODE" : "DARK MODE"}
+              </Button>
+            </Grid>
 
-        <Grid item container justify="center" xs={12}>
-          {this.state.users ? (
-            this.state.users.map((item, i) => (
-              <Grid>
-                <List>
-                  <ListItem alignItems="flex-start">
-                    <ListItemAvatar>
-                      <Avatar src={item.avatar} />
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary={
-                        <React.Fragment>
-                          <Typography
-                            component="span"
-                            variant="body2"
-                            color="textPrimary"
-                          >
-                            {item.first_name} {item.last_name}
-                          </Typography>
-                        </React.Fragment>
-                      }
-                      secondary={
-                        <React.Fragment>
-                          <Typography
-                            component="span"
-                            variant="body2"
-                            color="textPrimary"
-                          >
-                            {item.email}
-                          </Typography>
-                        </React.Fragment>
-                      }
-                    />
-                  </ListItem>
-                </List>
-              </Grid>
-            ))
-          ) : (
-            <CircularProgress disableShrink />
-          )}
-        </Grid>
-      </Grid>
+            <Grid item container justify="center" xs={12}>
+              {this.state.users ? (
+                this.state.users.map((item, i) => (
+                  <Grid>
+                    <List>
+                      <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                          <Avatar src={item.avatar} />
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={
+                            <React.Fragment>
+                              <Typography
+                                component="span"
+                                variant="body2"
+                                color="textPrimary"
+                              >
+                                {item.first_name} {item.last_name}
+                              </Typography>
+                            </React.Fragment>
+                          }
+                          secondary={
+                            <React.Fragment>
+                              <Typography
+                                component="span"
+                                variant="body2"
+                                color="textPrimary"
+                              >
+                                {item.email}
+                              </Typography>
+                            </React.Fragment>
+                          }
+                        />
+                      </ListItem>
+                    </List>
+                  </Grid>
+                ))
+              ) : (
+                <CircularProgress disableShrink />
+              )}
+            </Grid>
+          </Grid>
+        </Paper>
+      </ThemeProvider>
     );
   }
 }
